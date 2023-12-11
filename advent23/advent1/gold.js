c80e180b773ee52a912a1c6c63f584c0b6489c80
Aleksandra Markovic <aleksandra.markovic.1812@gmail.com>
2023-12-11 20:07:16 +0100

    Advent 1 gold

--- advent23/advent1/gold.js
+++ advent23/advent1/gold.js
@@ -14,9 +14,96 @@ lines.map((line) => {
 
   for (const index in line) {
     const ascii = line.charCodeAt(index);
+    const letter = line[index];
+
     if (ascii >= 48 && ascii <= 57) {
-      if (firstDigit == -1) firstDigit = line[index];
-      lastDigit = line[index];
+      if (firstDigit == -1) firstDigit = letter;
+      lastDigit = letter;
+      continue;
+    }
+
+    switch (letter) {
+      case "o": {
+        if (line[Number(index) + 1] == "n" && line[Number(index) + 2] == "e") {
+          if (firstDigit == -1) firstDigit = 1;
+          lastDigit = 1;
+        }
+        break;
+      }
+      case "t": {
+        if (line[Number(index) + 1] == "w" && line[Number(index) + 2] == "o") {
+          if (firstDigit == -1) firstDigit = 2;
+          lastDigit = 2;
+        }
+        if (
+          line[Number(index) + 1] == "h" &&
+          line[Number(index) + 2] == "r" &&
+          line[Number(index) + 3] == "e" &&
+          line[Number(index) + 4] == "e"
+        ) {
+          if (firstDigit == -1) firstDigit = 3;
+          lastDigit = 3;
+        }
+        break;
+      }
+      case "f": {
+        if (
+          line[Number(index) + 1] == "o" &&
+          line[Number(index) + 2] == "u" &&
+          line[Number(index) + 3] == "r"
+        ) {
+          if (firstDigit == -1) firstDigit = 4;
+          lastDigit = 4;
+        }
+        if (
+          line[Number(index) + 1] == "i" &&
+          line[Number(index) + 2] == "v" &&
+          line[Number(index) + 3] == "e"
+        ) {
+          if (firstDigit == -1) firstDigit = 5;
+          lastDigit = 5;
+        }
+        break;
+      }
+      case "s": {
+        if (line[Number(index) + 1] == "i" && line[Number(index) + 2] == "x") {
+          if (firstDigit == -1) firstDigit = 6;
+          lastDigit = 6;
+        }
+        if (
+          line[Number(index) + 1] == "e" &&
+          line[Number(index) + 2] == "v" &&
+          line[Number(index) + 3] == "e" &&
+          line[Number(index) + 4] == "n"
+        ) {
+          if (firstDigit == -1) firstDigit = 7;
+          lastDigit = 7;
+        }
+        break;
+      }
+      case "e": {
+        if (
+          line[Number(index) + 1] == "i" &&
+          line[Number(index) + 2] == "g" &&
+          line[Number(index) + 3] == "h" &&
+          line[Number(index) + 4] == "t"
+        ) {
+          if (firstDigit == -1) firstDigit = 8;
+          lastDigit = 8;
+        }
+        break;
+      }
+      case "n": {
+        if (
+          line[Number(index) + 1] == "i" &&
+          line[Number(index) + 2] == "n" &&
+          line[Number(index) + 3] == "e"
+        ) {
+          if (firstDigit == -1) firstDigit = 9;
+          lastDigit = 9;
+        }
+        break;
+      }
     }
   }
 
